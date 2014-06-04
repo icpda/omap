8fa0e7c4b0bda2065a72091a51f2cac8b0111c55
Israel Cepeda <israel4c@gmail.com>
2014-06-03 22:08:17 -0700

    input: misc: add missing header
    Add modules header to add module macros
    
    Signed-off-by: Israel Cepeda <israel4c@gmail.com>

--- drivers/input/misc/gp2a.c
+++ drivers/input/misc/gp2a.c
@@ -24,6 +24,7 @@
 #include <linux/device.h>
 #include <linux/delay.h>
 #include <linux/miscdevice.h>
+#include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/leds.h>
 #include <linux/gpio.h>
